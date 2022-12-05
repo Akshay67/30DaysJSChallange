10a2f057e0b185548596645db62416250e1ae31b
Akshay Shivekar <akshayshivekar06081995@gmail.com>
2022-12-05 12:23:04 -0500

    Day 01 challange

---
+++ datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof "India")
+console.log(typeof 7)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ variable.js
@@ -0,0 +1,22 @@
+// Lets Declare the variable and assign then different values
+
+let fname = 'Putin'
+let alive = true
+let myStatus
+let emptyValue = null
+
+
+let myFirstName = 'Peter'
+let myLastName = 'parker'
+let maritalStatus = 'married'
+const countryName = 'India'
+let age = 28
+
+let frName = 'Mark', lName = 'Doe', isMarried = true, country = 'Russia', theAge = 28
+
+
+let myAge = 28
+let yourAge = 37
+
+console.log("I am " + myAge + " years old")
+console.log("You are " + yourAge + " years old")
